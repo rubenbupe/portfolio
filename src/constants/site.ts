0df5ede81868c859d498789d3e78d8dc845e1b04
rubenbupe <rubenbupe@gmail.com>
2024-08-12 02:58:54 +0200

    feat: added desktop header

--- src/constants/site.ts
+++ src/constants/site.ts
@@ -4,3 +4,4 @@ export const siteDescription = 'Rubén Buzón - Full Stack Engineer';
 
 export const mailUrl = 'mailto:hello@rubenbupe.com';
 export const githubUrl = 'https://github.com/rubenbupe';
+export const linkedinUrl = 'https://www.linkedin.com/in/rubenbupe/';
